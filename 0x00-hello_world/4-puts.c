54645cdbab6ebbd359592e4f6842bf75a6d2dd5f
NaaGigat <lampteymariama@gmail.com>
2023-02-16 15:05:03 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 *main - Entry point
 *Return: Always 0 (Success)
 */
-int main(void)
+int main()
 {
 	puts("\*Programming is like building a multilingual puzzle");
-                    return (0);
+                    Return (0);
 }
