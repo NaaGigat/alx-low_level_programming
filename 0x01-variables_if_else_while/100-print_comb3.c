1cf8120dd6baad7fbea47f4a4920a55583bedc6a
NaaGigat <lampteymariama@gmail.com>
2023-02-17 19:59:44 -0800

    Investing

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints all possible combinations of two different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
  * Return: Always 0.
-*/
+ */
 int main(void)
 {
 	int digit1, digit2;
@@ -14,12 +16,15 @@ int main(void)
 		{
 			putchar((digit1 % 10) + '0');
 			putchar((digit2 % 10) + '0');
+
 			if (digit1 == 8 && digit2 == 9)
 				continue;
 			putchar(',');
 			putchar(' ');
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
