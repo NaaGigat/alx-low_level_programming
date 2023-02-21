0562c6cc42ea53520cf04c3d638d93927095acf3
NaaGigat <lampteymariama@gmail.com>
2023-02-21 12:56:30 -0800

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @naa1: The first integer to be added.
+ * @naa2: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int add(int naa1, int naa2)
+{
+	return (naa1 + naa2);
+}
