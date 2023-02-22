e2ef4afb7a83f2924150acebb38c0f960b39d90d
NaaGigat <lampteymariama@gmail.com>
2023-02-21 16:35:43 -0800

    I received an 'A'

---
+++ 0x02-functions_nested_loops/102-fibonacci_.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *        separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int mar;
+	
+	unsigned long naa = 0, naag = 1, sum;
+
+	for (mar = 0; mar < 50; mar++)
+	{
+		sum = naa + naag;
+		printf("%lu", sum);
+
+		naag = naag;
+		naag = sum;
+
+		if (mar == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
