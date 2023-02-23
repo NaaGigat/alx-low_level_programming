c10920eb669835836ccf879fd346a88e460909f8
NaaGigat <lampteymariama@gmail.com>
2023-02-23 06:00:39 -0800

    Line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - prints line
+ * @k: line
+ */
+void print_line(int k)
+{
+	int b = k;
+
+	while (b <= k)
+	{
+		_putchar(95);
+		b++;
+	}
+	_putchar('\n');
+}
+
