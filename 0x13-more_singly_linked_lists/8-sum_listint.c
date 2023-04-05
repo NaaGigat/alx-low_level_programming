a7949aff46667d7aa7018cb0b0589efed66adb80
NaaGigat <lampteymariama@gmail.com>
2023-04-04 18:53:02 -0700

    Calculates the sum of the data in a joined list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - determines the sum of all the data of a joined list
+ * @head: the list's firt node's pointer
+ * Return: sum of all the data (n) of the list, or 0 if the list is empty
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *node = head;
+
+	while (node != NULL)
+
+	{
+		sum += node->n;
+		node = node->next;
+	}
+
+	return (sum);
+}i
