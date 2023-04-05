ba1d822c1a15b1e7a7540626be683ad168fde196
NaaGigat <lampteymariama@gmail.com>
2023-04-04 19:12:13 -0700

    Reversing joined list

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a linked list
+ * @head: head node's initial pointer
+ * Return: initial node's pointer in the new list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+
+{
+
+listint_t *prev = NULL;
+	listint_t *next = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = prev;
+
+	return (*head);
+
+}
