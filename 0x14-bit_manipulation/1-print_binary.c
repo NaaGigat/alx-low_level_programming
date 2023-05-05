6e5307da7ec836b955a3903217f1951ceecd3966
NaaGigat <lampteymariama@gmail.com>
2023-05-04 17:32:23 -0700

    Binary rep of a number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - representation of a binary number
+ * @n: the number to print
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+        unsigned long int naag = 1UL << (sizeof(unsigned long int) * 8 - 1);
+        int flag = 0;
+
+        while (kosh > 0)
+        {
+                if ((n & kosh) == kosh)
+                {
+                        _putchar('1');
+                        flag = 1;
+                }
+                else if (flag == 1 || kosh == 1)
+                        _putchar('0');
+                kosh >>= 1;
+        }
+
+}
+
